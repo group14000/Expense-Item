19a90805cdded1ed7893c26b4ea292893e496aa2
DIGONTA HALDER <digantahalder14000@gmail.com>
2023-09-28 18:05:53 +0530

    Second commit

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,13 +1,29 @@
+import React from 'react';
 import ExpenseItem from './components/ExpenseItem';
 
-
 function App() {
-  return (
-    <div>
-      <h1>Expense Items</h1>
-      <ExpenseItem></ExpenseItem>
-    </div>
-  );
+    // Create an array of expenses
+    const expenses = [
+        { id: 1, description: 'Food', amount: 10, location: 'Home' },
+        { id: 2, description: 'Petrol', amount: 100, location: 'Gas Station' },
+        { id: 3, description: 'Movies', amount: 200, location: 'Cinema' },
+        // Add more expenses as needed
+    ];
+
+    return (
+        <div>
+            <h1>Expense Items</h1>
+            {/* Map through the expenses array and create ExpenseItem components */}
+            {expenses.map((expense) => (
+                <ExpenseItem
+                    key={expense.id}
+                    description={expense.description}
+                    amount={expense.amount}
+                    location={expense.location}
+                />
+            ))}
+        </div>
+    );
 }
 
 export default App;
--- first-app/src/components/ExpenseItem.js
+++ first-app/src/components/ExpenseItem.js
@@ -1,9 +1,18 @@
-function ExpenseItem () {
+import React from 'react';
+
+function ExpenseItem() {
+    // Define the LocationOfExpenditure variable
+    const LocationOfExpenditure = "Home"; // You can change this to the actual location
+
     return (
         <div>
-        <h3>Food Rs 10</h3>
-        <h3>Petrol Rs 100</h3>
-        <h3>Movies Rs 200</h3>
+            {/* Display the LocationOfExpenditure on the screen */}
+            <h2>Location of Expenditure: {LocationOfExpenditure}</h2>
+
+            {/* List of expenses */}
+            <h3>Food Rs 10</h3>
+            <h3>Petrol Rs 100</h3>
+            <h3>Movies Rs 200</h3>
         </div>
     );
 }
