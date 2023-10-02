42c3e8d503c8d4d71037520b8ce75939a6162636
DIGONTA HALDER <digantahalder14000@gmail.com>
2023-10-02 11:29:53 +0530

    Challenges on Conditional Rendering

--- first-app/src/components/Expenses/ExpenseDate.js
+++ first-app/src/components/Expenses/ExpenseDate.js
@@ -1,17 +1,12 @@
-// Import React library
 import React from 'react';
 
-// Import CSS styles for the ExpenseDate component
 import './ExpenseDate.css';
 
-// Define the ExpenseDate functional component that takes 'props' as an argument
 const ExpenseDate = (props) => {
-  // Extract the month, day, and year from the 'date' prop and format them
   const month = props.date.toLocaleString('en-US', { month: 'long' });
   const day = props.date.toLocaleString('en-US', { day: '2-digit' });
   const year = props.date.getFullYear();
 
-  // Return JSX for rendering the formatted date components
   return (
     <div className='expense-date'>
       <div className='expense-date__month'>{month}</div>
@@ -21,5 +16,4 @@ const ExpenseDate = (props) => {
   );
 };
 
-// Export the ExpenseDate component as the default export
 export default ExpenseDate;
